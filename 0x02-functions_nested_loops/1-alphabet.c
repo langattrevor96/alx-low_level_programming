dac62250fb90935cec02cd447724a4c51ed23684
langattrevor96 <langattrevor96@gmail.com>
2022-09-18 09:42:19 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphaber in lowercase
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	char = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
