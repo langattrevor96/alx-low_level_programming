2521ff1179ef0461c9d58fbc9fc83b105241fc8d
langattrevor96 <langattrevor96@gmail.com>
2022-09-22 09:36:07 -0700

    This is leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337
+ * letters a and A are replaced by 4
+ * letters l and L are replaced by 1
+ * letters t and T are replaced by 7
+ * @s: pointer to string
+ * Return: pointer to s
+ */
+char *leet(char *s)
+{
+	int stringCount, leetCount;
+	char leetLetters[] = "aAeEoOtTlL";
+	char leetNums[] = "4433007711";
+
+	stringCount = 0;
+	while (s[stringCount] != '\0')
+	{
+		leetCount = 0;
+		while (leetCount < 10)
+		{
+			if (leetLetters[leetCount] == s[stringCount])
+			{
+				s[stringCount] = leetNums[leetCount];
+			}
+			leetCount++;
+		}
+		stringCount++;
+	}
+	return (s);
+}
