4c1f0be6b2bc9beb41ead0366ba2fe39415974fb
langattrevor96 <langattrevor96@gmail.com>
2022-09-22 04:03:02 -0700

    This is task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,17 +7,24 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int length, j;
+	int i;
+	int j;
 
-length = 0;
-while (dest[length] != '\0')
-{
-length++;
-}
-for (j = 0; src[j] != '\0'; j++, length++)
-{
-dest[length] = src[j];
-}
-dest[length] = '\0';
-return (dest);
+	m = 0;
+	n = 0;
+
+	while (dest[m] != '\0')
+	{
+		m++;
+	}
+
+	while (src[n] != '\0')
+	{
+		dest[m] = src[n];
+		n++;
+		m++;
+	}
+
+	dest[m] = '\0';
+	return (dest);
 }
