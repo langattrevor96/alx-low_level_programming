6f357e086ae57306bcf219cab5714f26ba842666
langattrevor96 <langattrevor96@gmail.com>
2022-09-22 09:57:33 -0700

    This is task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_number  - prints an integer
+ * using the putchar function
+ * @n: integer to be printed
+ * return: void
+ */
+void print_number(int n)
+{
+	unsigned int  num;
+
+	num = n;
+
+	if (n < 0)
+	{
+		_putchar(81);
+		num = -n;
+	}
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
