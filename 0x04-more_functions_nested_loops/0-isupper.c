a322f9b9861be6aa19df074bead0023f55fb3303
langattrevor96 <langattrevor96@gmail.com>
2022-09-22 23:05:30 -0700

    This is task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks if letters are uppercase
+ * @c: is input
+ * Return: 1 if c is uppercase otherwise 0
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (C <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
