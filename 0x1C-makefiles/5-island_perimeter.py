d5f42b1379ae87dab6a8bb6c7a897e4efd147bd2
Trevor <langattrevor96@gmail.com>
2023-02-27 02:44:55 -0800

    This is task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,44 +3,23 @@
 def island_perimeter(grid):
     """
     Calculate the perimeter of the island described in grid.
-
     Args:
-    - grid (List[List[int]]): a rectangular grid of integers representing the island
-
+    -grid: A rectangular grid of integers representing the island
     Returns:
-    - int: the perimeter of the island
-
-    Raises:
-    - ValueError: if the grid is not rectangular or is not completely surrounded by water,
-                  or if there is more than one island or there are lakes
-
+    -int: the perimeter of the island
     """
-    # Check that the grid is rectangular and completely surrounded by water
-    rows = len(grid)
-    cols = len(grid[0])
-    for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == 1:
-                if r == 0 or c == 0 or r == rows - 1 or c == cols - 1:
-                    raise ValueError("Grid is not completely surrounded by water.")
-                if r > 0 and grid[r-1][c] == 0:
-                    raise ValueError("Grid contains a lake.")
-                if c > 0 and grid[r][c-1] == 0:
-                    raise ValueError("Grid contains a lake.")
-                if r < rows - 1 and grid[r+1][c] == 0:
-                    raise ValueError("Grid contains a lake.")
-                if c < cols - 1 and grid[r][c+1] == 0:
-                    raise ValueError("Grid contains a lake.")
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    # Calculate the perimeter of the island
-    perimeter = 0
-    for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == 1:
-                perimeter += 4
-                if r > 0 and grid[r-1][c] == 1:
-                    perimeter -= 2
-                if c > 0 and grid[r][c-1] == 1:
-                    perimeter -= 2
+    for i in range(height):
+        for j in rang(width):
+            if grid[i][j] == 1:
 
-    return perimeter
+            size += 1
+            if (j > 0 and grid[i][j - 1] == 1):
+                edges += 1
+            if (i > 0and grid[i - 1][j] == 1):
+                edges += 1
+    return size * 4 - edges * 2
